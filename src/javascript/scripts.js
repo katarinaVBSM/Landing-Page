f722c836b1515acae8cf2e2b7fd9475320d5dfd5
katarinaVBSM <katarinavitoria36@gmail.com>
2024-09-16 15:17:05 -0300

    Landing page

---
+++ src/javascript/scripts.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+    $('#mobile_btn').on('click',function(){
+        $('#mobile_menu').toggleClass('active');
+        $('#mobile_btn').find('i').toggLeClass('fa-x');
+    });
+
+    const sections = $('section');
+    const navItems = $('.nav-item');
+
+    $(window).on('scroll', function (){
+        const header = $('header');
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+
+        let activeSectionIndex = 0;
+
+       if (scrollPosition <= 0){
+           header.css('box-shadow', 'none');
+       } else {
+         header.css('box-shadow','5px 1px 5px rgba(0,0,0,0.1)');
+       }
+       sections.each(function(i) {
+        const section = $(this);
+        const sectionTop = section.offset().top - 96;
+        const sectionBottom = sectionTop + section.outerHeight();
+
+        if(scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+            activeSectionIndex = i;
+            return false;
+        }
+       });
+
+       navItems.removeClass('active');
+       $(navItems [activeSectionIndex]).addClass('active');
+     });
+
+     ScrollReveal().reveal('#cta',{
+        origin:'Bottom',
+        duration:2000,
+        distance:'2%'
+     });
+
+     ScrollReveal().reveal('.dish',{
+        origin:'Bottom',
+        duration:2000,
+        distance:'2%'
+     });
+
+     ScrollReveal().reveal('#testimonial_chef',{
+        origin:'Bottom',
+        duration:1000,
+        distance:'2%'
+     });
+
+     ScrollReveal().reveal('.feedback',{
+        origin:'right',
+        duration:2000,
+        distance:'2%'
+     });
+});
